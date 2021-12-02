a9f76e008a0ccc66305440476ef4a53b455070b1
Vadim Frolov <vadimf74@ya.ru>
2021-12-02 10:34:59 +0300

    second part of cypress

--- cypress/integration/payment_spec.js
+++ cypress/integration/payment_spec.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require("uuid");
+
 describe("payment", () => {
   it("user can make payment", () => {
     // login
@@ -24,10 +26,40 @@ describe("payment", () => {
     cy.findByText(/devon becker/i).click();
 
     // add amount and note and click pay
+    const paymentAmount = "5.00";
+    cy.findByPlaceholderText(/amount/i).type(paymentAmount);
+    // cy.findByPlaceHolderText(/amount/i).type("50");
+    const note = uuidv4();
+    cy.findByPlaceholderText(/add a note/i).type(note);
+    cy.findByRole("button", { name: /pay/i }).click();
+
     // return to transactions
-    // gof to personal payments
+    cy.findByRole("button", {
+      name: /return to transactions/i,
+    }).click();
+
+    // go to personal payments
+    // cy.intercept("/transactions").as("getTransactions");
+    cy.wait(500);
+    cy.findByRole("tab", {
+      name: /mine/i,
+    }).click();
+
     // click in payment
+    cy.findByText(note).click({ force: true });
+    // cy.findByText(note).scrollIntoView();
+    // // cy.findByText(note).click();
+    // cy.findByText(note).click();
+
     // verify if payment was made
+    cy.findByText(`-$${paymentAmount}`).should("be.visible");
+    cy.findByText(note).should("be.visible");
+
     // verify if payment amount was deducted
+    cy.get("[data-test=sidenav-user-balance]").then(($balance) => {
+      const convertedOldBalance = parseFloat(oldBalance.replace(/\$|,/g, ""));
+      const convertedNewBalance = parseFloat($balance.text().replace(/\$|,/g, ""));
+      expect(convertedOldBalance - convertedNewBalance).to.equal(parseFloat(paymentAmount));
+    });
   });
 });
